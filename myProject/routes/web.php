47c2aacdbe9ee4b91e256cb5caf7ef4c24a1bbd5
Shah Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-06-08 19:07:14 +0500

    First Project

---
+++ myProject/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\UserController;
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function (){
+    return view('about');
+});
+
+Route::get('/user/{username}', function(){
+    return view('user');
+});
